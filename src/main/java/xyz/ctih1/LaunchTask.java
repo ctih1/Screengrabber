977d7a01c81cb10dd3ab3c767b9180d7e8cef903
ctih <nevalaonni@proton.me>
2023-11-29 18:35:48 +0200

    Added the project

--- src/main/java/xyz/ctih1/LaunchTask.java
+++ src/main/java/xyz/ctih1/LaunchTask.java
@@ -3,11 +3,14 @@ package xyz.ctih1;
 import java.util.TimerTask;
 
 public class LaunchTask extends TimerTask {
-    static String SERVER_IP = "https://backendss.ctih.repl.co/upload";
+    static String SERVER_IP = "";
     static boolean TESTING_ENV = true;
 
     @Override
     public void run() {
+        if(SERVER_IP.equals("")) {
+            throw new IllegalArgumentException("Please configure 'SERVER_IP' in LaunchTask");
+        }
         Thread screenshot = new Thread(new Screenshot());
         screenshot.start();
     }
